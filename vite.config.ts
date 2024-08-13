02ef82244534e33bf11ffeebfed496333c1559c5
opase <opase@qq.com>
2024-08-13 16:16:33 +0800

    修改部署服务器地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   server: {
     base: "/admin",
     port: 3003,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     open: true,
   },
   resolve: {
     alias: {
-      '@': '/src/'
+      "@": "/src/",
     },
   },
 
   plugins: [vue()],
-})
+});
